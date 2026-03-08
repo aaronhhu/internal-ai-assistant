aa30745004addfa94d55afe37a73128a8f21ef4f
Aaron Hu <ahhu@ucdavis.edu>
2026-03-08 12:31:20 -0700

    initial FASTAPI setup

---
+++ test/test_main.py
@@ -0,0 +1,9 @@
+from src.main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+def test_root():
+    r = client.get("/")
+    assert r.status_code == 200
+    assert r.json() == {"message": "Hello, World!"}
